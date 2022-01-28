e0ea72e7dfcc3abb7e03b17e2fce94f97ccaf2e5
Jobson Ribeiro de Souza <jobsonrisoa@gmail.com>
2022-01-27 22:54:50 -0300

    Devo loggar consultas

--- src/app.js
+++ src/app.js
@@ -22,4 +22,10 @@ app.get('/', (req, res) => {
   res.status(200).send();
 });
 
+// app.db.on('query', (query) => {
+//  console.log({ sql: query.sql, bindings: query.bindings ? query.bindings.join(',') : '' });
+// }).on('query-response', ((response) => {
+//  console.log(response);
+// })).on('error', (error) => console.log(error));
+
 module.exports = app;
